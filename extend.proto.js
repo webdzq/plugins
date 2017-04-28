d39ef37762996d7be5e10970f7422247541ceb99
dongzhengqi <dzq258@126.com>
2017-04-28 09:38:24 +0800

    add files

--- extend.proto.js
+++ extend.proto.js
@@ -66,3 +66,35 @@ var fabonacci = momozier([0, 1], function(recur, n) {
 var fabonacci_1 = momosizer([1, 1], function(recur, n) {
     return n * recur(n - 1);
 });
+
+//高阶函数实现AOP(面向切面编程)
+Function.prototype.before = function(beforefn) {
+    var __self = this; // 保存原函数的引用
+    return function() { // 返回包含了原函数和新函数的"代理"函数
+        beforefn.apply(this, arguments);
+        return __self.apply(this, arguments);
+    };
+};
+Function.prototype.after = function(afterfn) {
+    var __self = this;
+    return function() {
+        // 执行新函数,修正 this // 执行原函数
+
+        var ret = __self.apply(this, arguments);
+
+        afterfn.apply(this, arguments);
+        return ret;
+    };
+};
+var func = function() {
+    console.log(2);
+};
+func = func.before(function() {
+    console.log(1);
+}).after(function() {
+    console.log(3);
+}).before(function() {
+    console.log(4);
+});
+//console.log(func);
+func(); //4,1,2,3-------解析：注意执行顺序。
